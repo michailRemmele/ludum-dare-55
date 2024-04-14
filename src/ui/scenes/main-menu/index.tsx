5b0c2f28bdb84deba3aefc65ddcc64c478d17734
yepstepz <prettyoptimist@gmail.com>
2024-04-14 18:33:00 +0300

    add menu to main screen

--- src/ui/scenes/main-menu/index.tsx
+++ src/ui/scenes/main-menu/index.tsx
@@ -27,6 +27,43 @@ export const MainMenu: FC = () => {
         className="main-menu__logo"
       />
       <button className="main-menu__button" type="button" onClick={handlePlay}>Play</button>
+      <ul className="action-list">
+        <li className="action-list__item">
+          <div className="action-button">
+            <span className="action-button__key">W</span>
+            <span className="action-button__key">A</span>
+            <span className="action-button__key">S</span>
+            <span className="action-button__key">D</span>
+            <div className="action-button__label">
+              Move
+            </div>
+          </div>
+        </li>
+        <li className="action-list__item">
+          <div className="action-button">
+            <span className="action-button__key">E</span>
+            <div className="action-button__label">
+              Resurrect
+            </div>
+          </div>
+        </li>
+        <li className="action-list__item">
+          <div className="action-button">
+            <span className="action-button__key">Q</span>
+            <div className="action-button__label">
+              Summon
+            </div>
+          </div>
+        </li>
+        <li className="action-list__item">
+          <div className="action-button">
+            <span className="action-button__key action-button__key--space">SPACE</span>
+            <div className="action-button__label">
+              Jump
+            </div>
+          </div>
+        </li>
+      </ul>
     </div>
   );
 };
